017b8a23bfd3435be62b26ff59738aa56054e140
BAProductions <anacastellano@bellsouth.net>
2013-01-04 12:02:17 -0500

    Update Add_Player_Function.js

--- Add_Player_Function.js
+++ Add_Player_Function.js
@@ -1,1 +1,26 @@
-s
+jQuery.fn.addVideo = function(source,thumb,auto){
+  var s = "";
+  var t = "";
+	var a = "";
+	if (auto == true) {
+		a = 'autoplay="autoplay"';
+	}else{
+		a = "";
+	}
+	if (thumb !== "") {
+		t = 'poster="'+thumb+'"';
+	}else{
+		t = "";
+    }
+	if (source !== "") {
+		s = source;
+	}else{
+		s = "";
+	}
+	$(this).append('<video class="screen" id="video"' + t + 'autobuffer preload="auto" controls="controls">\
+    <source src="' + s + '.mp4" type="video/mp4">\
+    <source src="' + s + '.ogg" type="video/ogg">\
+	<source src="' + s + '.webm" type="type="video/webm">\
+	<object data="' + s + '"></object>\
+  </video>\
+  ');
